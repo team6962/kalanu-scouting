1ecb31b380afc784e53caacaf03b8a0189c88c22
Soren <soren@aethre.co>
2023-03-03 12:34:33 -0800

    new service worker to improve online performance

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,7 +1,8 @@
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { NetworkFirst } from 'workbox-strategies';
+import { CacheFirst } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
 
 declare let self: ServiceWorkerGlobalScope;
 
@@ -9,9 +10,13 @@ precacheAndRoute(self.__WB_MANIFEST);
 
 registerRoute(
 	({ url }) => url.href.startsWith('https://www.thebluealliance.com/api/v3'),
-	new NetworkFirst({
+	new CacheFirst({
 		cacheName: 'tba-cache',
 		plugins: [
+			new ExpirationPlugin({
+				maxEntries: 20,
+				maxAgeSeconds: 4 * 24 * 60 * 60
+			}),
 			new CacheableResponsePlugin({
 				statuses: [0, 200]
 			})
