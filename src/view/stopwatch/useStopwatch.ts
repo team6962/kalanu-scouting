1bdab4d44c510a8b2f9f2b598e074efed8b85735
Soren <soren@aethre.co>
2023-03-09 16:05:41 -0800

    fix safari styling

--- src/view/stopwatch/useStopwatch.ts
+++ src/view/stopwatch/useStopwatch.ts
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * creates a stopwatch that periodically updates a time value
+ * @param interval the delay between updates, in milliseconds
+ * @param initialStart the starting timestamp. if not provided, will default to `Date.now()`
+ * @returns seconds since start
+ */
 export const useStopwatch = (interval = 1000, initialStart?: number) => {
 	const [start] = useState(initialStart || Date.now());
 	const [time, setTime] = useState(0);
